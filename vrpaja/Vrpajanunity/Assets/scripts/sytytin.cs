552b5470f73700905b25b73177f914ee3849064e
eppumelto <eppu.melto@hotmail.fi>
2021-04-16 12:52:02 +0300

    added urp #2

---
+++ vrpaja/Vrpajanunity/Assets/scripts/sytytin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sytytin : MonoBehaviour
+{
+    public GameObject Laatikko;
+
+    public void Shoot ()
+    {
+        Laatikko.SetActive(true);
+    }
+}
