c28a4295939ad70e60c16ffa48f00808b19e3f63
eppumelto <eppu.melto@hotmail.fi>
2021-04-23 09:26:35 +0300

    hiilispawnia

---
+++ vrpaja/Vrpajanunity/Assets/scripts/hiilisakki.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hiilisakki : MonoBehaviour
+{
+    public GameObject coal;
+    public Transform spawnPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Spawn ()
+    {
+        Instantiate(coal, spawnPos);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
