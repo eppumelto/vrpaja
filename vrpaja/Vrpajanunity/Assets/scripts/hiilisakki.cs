d1a7cf5fe16b46d1e7edf13fe11fa34c0ffe6fd5
mollaaja <jesse.pirinen@koudata.fi>
2021-04-29 10:01:16 +0300

    Koitin pelastaa koodia

--- vrpaja/Vrpajanunity/Assets/scripts/hiilisakki.cs
+++ vrpaja/Vrpajanunity/Assets/scripts/hiilisakki.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class hiilisakki : MonoBehaviour
 {
@@ -11,17 +12,22 @@ public class hiilisakki : MonoBehaviour
     private float timer = 1f;
     public float timerAika = 10f;
     public int limit = 25;
-
+    //jos ei toimi voi poistaa
+    private bool reference;
 
     void Start()
     {
+        //jos ei toimi voi poistaa
+        reference = GameObject.Find("Hiilisakki").GetComponent<XRGrabInteractable>().trackPosition;
         
     }
 
+
+    //Eli kun se trackaa sen positiota LUULEN että se on kädessä ja silloin kaikki toimii
     void Spawn ()
     {
         //tee timer mikä spawnaa hiiltä
-        if(timer <= 0)
+        if (timer <= 0 && reference == true)
         {
             Instantiate(coal, spawnPos.transform.position, spawnPos.transform.rotation);
             timer = timerAika;
@@ -30,25 +36,18 @@ public class hiilisakki : MonoBehaviour
     }
 
 
-    public void Kadessa()
+    
+    void Update()
     {
-        Debug.Log("Kadessa");
+
         if (transform.eulerAngles.z >= 80 && transform.eulerAngles.z <= 180 && limit >= 0)
         {
             InvokeRepeating("Spawn", 0, 0);
         }
 
         timer = timer - Time.deltaTime;
-    }
-    
-    void Update()
-    {
-        
 
-        //if (transform.eulerAngles.y >= 90 && transform.eulerAngles.y <= 180)
-        //{
-        //    InvokeRepeating("Spawn", time, delay);
-        //}
+  
 
     }
 }
