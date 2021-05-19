e983ad4c3ea57dde2c82d2295149c0359e4f6715
mollaaja <jesse.pirinen@koudata.fi>
2021-05-19 09:43:02 +0300

    materials kansio siirretty

---
+++ vrpaja/Vrpajanunity/Assets/scripts/handAnim.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class handAnim : MonoBehaviour
+{
+    Animator animator;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    internal void SetGrip(float v)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void SetTrigger(float v)
+    {
+        throw new NotImplementedException();
+    }
+
+    void AnimateHand()
+    {
+        if(gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent,gripTarget, Time.deltaTime * speed);
+            animator.SetFloat("fist", gripCurrent);
+        }
+        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            animator.SetFloat("index", triggerCurrent);
+        }
+
+    }
+}
